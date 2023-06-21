aae8a57a9d76fc255bd8a7b706bc1520fd67fa25
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-06-21 13:33:10 +0100

    Update CI and tests ready for test deployment

--- tasks/test-package.js
+++ tasks/test-package.js
@@ -3,48 +3,79 @@ const packageJson = require("../package.json");
 
 let failure = null;
 
+const componentJavascriptFiles = (component) => [
+  `nationalarchives/components/${component}/${component}.js`,
+  `nationalarchives/components/${component}/${component}.js.map`,
+  `nationalarchives/components/${component}/${component}.mjs`,
+];
+
+const componentFiles = (component, excludeJavaScript = false) => [
+  `nationalarchives/components/${component}`,
+  `nationalarchives/components/${component}/_${component}.scss`,
+  `nationalarchives/components/${component}/_index.scss`,
+  `nationalarchives/components/${component}/fixtures.json`,
+  `nationalarchives/components/${component}/macro-options.json`,
+  `nationalarchives/components/${component}/macro.njk`,
+  `nationalarchives/components/${component}/template.njk`,
+  ...(excludeJavaScript ? [] : componentJavascriptFiles(component)),
+];
+
 const packageDirectory = "package";
 const checkExists = [
   "",
+  // Root files
   "package.json",
   "govuk-prototype-kit.config.json",
   "README.md",
   "nationalarchives",
-  "nationalarchives/assets",
-  "nationalarchives/assets/images/favicon.ico",
-  "nationalarchives/assets/images/tna-horizontal-logo.svg",
-  "nationalarchives/assets/images/tna-horizontal-logo-inverted.svg",
-  "nationalarchives/assets/images/tna-square-logo.svg",
-  "nationalarchives/assets/images/tna-square-logo-inverted.svg",
-  "nationalarchives/components",
-  // "nationalarchives/components/header/_header.scss",
-  // "nationalarchives/components/header/_index.scss",
-  // "nationalarchives/components/header/fixtures.json",
-  // "nationalarchives/components/header/header.js",
-  // "nationalarchives/components/header/header.mjs",
-  // "nationalarchives/components/header/header.stories.js",
-  // "nationalarchives/components/header/macro-options.json",
-  // "nationalarchives/components/header/macro.njk",
-  // "nationalarchives/components/header/template.njk",
-  "nationalarchives/components/grid/_grid.scss",
-  "nationalarchives/components/grid/_index.scss",
-  "nationalarchives/components/grid/fixtures.json",
-  "nationalarchives/components/grid/grid.stories.js",
-  "nationalarchives/components/grid/macro-options.json",
-  "nationalarchives/components/grid/macro.njk",
-  "nationalarchives/components/grid/template.njk",
   "nationalarchives/_base.scss",
   "nationalarchives/_prototype-kit.scss",
   "nationalarchives/all.css",
+  "nationalarchives/all.css.map",
   "nationalarchives/all.js",
+  "nationalarchives/all.js.map",
   "nationalarchives/all.mjs",
   "nationalarchives/all.scss",
+  // Assets
+  "nationalarchives/assets",
+  "nationalarchives/assets/images/apple-touch-icon-152x152.png",
+  "nationalarchives/assets/images/apple-touch-icon-167x167.png",
+  "nationalarchives/assets/images/apple-touch-icon-180x180.png",
+  "nationalarchives/assets/images/apple-touch-icon.png",
+  "nationalarchives/assets/images/favicon.ico",
+  "nationalarchives/assets/images/mask-icon.svg",
+  "nationalarchives/assets/images/nationalarchives-opengraph-image.png",
+  "nationalarchives/assets/images/tna-horizontal-logo-inverted.svg",
+  "nationalarchives/assets/images/tna-horizontal-logo.svg",
+  "nationalarchives/assets/images/tna-square-logo-inverted.svg",
+  "nationalarchives/assets/images/tna-square-logo.svg",
+  // Components
+  "nationalarchives/components",
+  ...componentFiles("button", true),
+  ...componentFiles("card", true),
+  ...componentFiles("footer", true),
+  ...componentFiles("grid", true),
+  ...componentFiles("sensitive-image"),
+  // Tools
+  "nationalarchives/tools",
+  "nationalarchives/tools/_all.scss",
+  "nationalarchives/tools/_exports.scss",
+  // Utilities
+  "nationalarchives/utilities",
+  "nationalarchives/utilities/_all.scss",
+  "nationalarchives/utilities/_global.scss",
+  "nationalarchives/utilities/_grid.scss",
+  "nationalarchives/utilities/_typography.scss",
+  // Variables
+  "nationalarchives/variables",
+  "nationalarchives/variables/_all.scss",
+  "nationalarchives/variables/_grid.scss",
+  "nationalarchives/variables/_media.scss",
 ];
 
 console.log(`Testing package file structure`);
 checkExists.forEach((checkFile) => {
   const checkFilePath = `${packageDirectory}/${checkFile}`;
-
   try {
     fs.accessSync(checkFilePath);
     console.log(
@@ -100,7 +131,14 @@ expectedPrototypeKitConfigProperties.forEach(
   }
 );
 
+// console.log("------------------------------------------");
+
 // TODO: Test CSS and JS for contents
+// console.log(`Testing compiled JavaScript file`);
+// const jsPackage = require("../package/nationalarchives/all.js")
+// console.log(jsPackage)
+// const {initAll} = require("../package/nationalarchives/all.mjs")
+// console.log(initAll)
 
 if (failure !== null) {
   process.exit(failure);

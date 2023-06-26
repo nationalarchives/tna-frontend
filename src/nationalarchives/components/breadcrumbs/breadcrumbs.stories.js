f84fd51c33ba10e729ed7a304d7746748cbfdc67
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-06-26 11:39:22 +0100

    Add breadcrumbs component

---
+++ src/nationalarchives/components/breadcrumbs/breadcrumbs.stories.js
@@ -0,0 +1,57 @@
+import Breadcrumbs from "./template.njk";
+import "./_index.scss";
+import macroOptions from "./macro-options.json";
+
+const argTypes = {
+  items: { control: "object" },
+  classes: { control: "text" },
+  attributes: { control: "text" },
+};
+
+Object.keys(argTypes).forEach((argType) => {
+  argTypes[argType].description = macroOptions.find(
+    (option) => option.name === argType
+  )?.description;
+});
+
+export default {
+  title: "Components/Breadcrumbs",
+  argTypes,
+};
+
+const Template = ({ items, classes, attributes }) => {
+  return Breadcrumbs({
+    params: {
+      items,
+      classes,
+      attributes,
+    },
+  });
+};
+
+export const Standard = Template.bind({});
+Standard.args = {
+  items: [
+    {
+      text: "Alpha",
+      href: "#/alpha",
+    },
+    {
+      text: "Beta",
+      href: "#/beta",
+    },
+    {
+      text: "Gamma",
+      href: "#/gamma",
+    },
+    {
+      text: "Delta",
+      href: "#/delta",
+    },
+    {
+      text: "Epsilon",
+      href: "#/epsilon",
+    },
+  ],
+  classes: "tna-breadcrumbs--demo",
+};

25341470bbde16fbeb6aee2b486e0c72cd552968
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-07-13 17:16:49 +0100

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": ["eslint:recommended", "plugin:storybook/recommended"],
+  "overrides": [{
     "env": {
-        "browser": true,
-        "es2021": true
+      "node": true
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
+    "files": [".eslintrc.{js,cjs}"],
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
+      "sourceType": "script"
     }
-}
+  }],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {}
+};

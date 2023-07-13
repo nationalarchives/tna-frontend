c8531cf26fbc31520b6b60cf770e1a4a6251dfe3
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-07-13 14:15:37 +0100

    Add HTML interaction modifier classes

--- src/nationalarchives/all.mjs
+++ src/nationalarchives/all.mjs
@@ -1,8 +1,48 @@
 import { Header } from "./components/header/header.mjs";
 import { SensitiveImage } from "./components/sensitive-image/sensitive-image.mjs";
 
-document.documentElement.classList.remove("js-disabled");
-document.documentElement.classList.add("js-enabled");
+const $body = document.documentElement;
+
+$body.classList.remove("tna-template--js-disabled");
+$body.classList.add("tna-template--js-enabled");
+
+const onFirstTouch = () => {
+  window.removeEventListener("touchstart", onFirstTouch);
+  $body.classList.add("tna-template--touched");
+};
+
+const onKeyDown = (e) => {
+  if (e.key === "Tab") {
+    $body.classList.add("tna-template--tabbed");
+    $body.classList.remove("tna-template--clicked");
+  }
+};
+
+const onMouseDown = () => {
+  $body.classList.add("tna-template--clicked");
+  $body.classList.remove("tna-template--tabbed");
+};
+
+window.addEventListener("touchstart", onFirstTouch);
+window.addEventListener("keydown", onKeyDown);
+window.addEventListener("mousedown", onMouseDown);
+
+
+
+if (document.documentElement.classList.contains('tna-template--system-theme')) {
+  if (window.matchMedia('(prefers-contrast: more)').matches) {
+    document.documentElement.classList.add('tna-template--high-contrast-theme')
+  } else {
+    document.documentElement.classList.add(
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'tna-template--dark-theme'
+        : 'tna-template--light-theme'
+    )
+  }
+}
+
+
+
 
 const initAll = (options) => {
   options = typeof options !== "undefined" ? options : {};

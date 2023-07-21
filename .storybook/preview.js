d7ee2e26b96b9da2da60daca31574605a2e6db43
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-07-21 14:14:23 +0100

    Add more viewport size options

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "../src/nationalarchives/all.scss";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 document.documentElement.classList.add("tna-template");
 document.documentElement.classList.add("tna-template--system-theme");
@@ -7,8 +8,27 @@ if (window.self !== window.top) {
 }
 document.body.classList.add("tna-template__body");
 
+const customViewports = {
+  ...INITIAL_VIEWPORTS,
+  // kindleFire2: {
+  //   name: "Kindle Fire 2",
+  //   styles: {
+  //     width: "600px",
+  //     height: "963px",
+  //   },
+  // },
+  // kindleFireHD: {
+  //   name: "Kindle Fire HD",
+  //   styles: {
+  //     width: "533px",
+  //     height: "801px",
+  //   },
+  // },
+};
+
 export const parameters = {
   actions: {
     disable: true,
   },
+  viewport: { viewports: customViewports },
 };

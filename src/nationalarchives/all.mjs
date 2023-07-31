5930376961e6fde84eba1c065f927091654eda81
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-07-31 15:57:21 +0100

    Support for compound light/dark and high contrast

--- src/nationalarchives/all.mjs
+++ src/nationalarchives/all.mjs
@@ -30,14 +30,13 @@ window.addEventListener("keydown", onKeyDown);
 window.addEventListener("mousedown", onMouseDown);
 
 if (document.documentElement.classList.contains("tna-template--system-theme")) {
+  document.documentElement.classList.add(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "tna-template--dark-theme"
+      : "tna-template--light-theme",
+  );
   if (window.matchMedia("(prefers-contrast: more)").matches) {
     document.documentElement.classList.add("tna-template--high-contrast-theme");
-  } else {
-    document.documentElement.classList.add(
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "tna-template--dark-theme"
-        : "tna-template--light-theme",
-    );
   }
 }
 

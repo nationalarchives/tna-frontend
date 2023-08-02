d2c485a201ac4a6ae155f863803ce56dab8a92f3
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-08-02 09:41:13 +0100

    Require hero image, support sizes and add fixture

--- src/nationalarchives/components/hero/hero.stories.js
+++ src/nationalarchives/components/hero/hero.stories.js
@@ -43,6 +43,8 @@ Standard.args = {
   image: {
     src: "https://www.nationalarchives.gov.uk/wp-content/uploads/sites/24/2023/07/tna-building-compress.jpg",
     alt: "The National Archives office",
+    width: 1996,
+    height: 1331,
     information: "An interesting photo by a famous photographer ©2023",
   },
   classes: "tna-hero--demo",
@@ -81,6 +83,8 @@ TitleOnly.args = {
   image: {
     src: "https://www.nationalarchives.gov.uk/wp-content/uploads/sites/24/2023/07/tna-building-compress.jpg",
     alt: "The National Archives office",
+    width: 1996,
+    height: 1331,
   },
   classes: "tna-hero--demo",
 };

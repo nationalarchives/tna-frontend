3fdad1059bba5a26864e2571aa800d719a90dbe9
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-08-08 10:45:14 +0100

    Create draft profile component

---
+++ src/nationalarchives/components/profile/profile.stories.js
@@ -0,0 +1,32 @@
+import Profile from "./template.njk";
+import macroOptions from "./macro-options.json";
+
+const argTypes = {
+  classes: { control: "text" },
+  attributes: { control: "object" },
+};
+
+Object.keys(argTypes).forEach((argType) => {
+  argTypes[argType].description = macroOptions.find(
+    (option) => option.name === argType,
+  )?.description;
+});
+
+export default {
+  title: "Work in progress/Components/Profile",
+  argTypes,
+};
+
+const Template = ({ classes, attributes }) => {
+  return Profile({
+    params: {
+      classes,
+      attributes,
+    },
+  });
+};
+
+export const Standard = Template.bind({});
+Standard.args = {
+  classes: "tna-profile--demo",
+};

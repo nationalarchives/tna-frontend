ebce031164fa12d3391e9460f3081a3f0a1a8381
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-08-08 15:41:46 +0100

    Add draft message component

---
+++ src/nationalarchives/components/message/phase-banner.stories.js
@@ -0,0 +1,34 @@
+import Message from "./template.njk";
+import macroOptions from "./macro-options.json";
+
+const argTypes = {
+  message: { control: "text" },
+  classes: { control: "text" },
+  attributes: { control: "object" },
+};
+
+Object.keys(argTypes).forEach((argType) => {
+  argTypes[argType].description = macroOptions.find(
+    (option) => option.name === argType,
+  )?.description;
+});
+
+export default {
+  title: "Components/Message",
+  argTypes,
+};
+
+const Template = ({ phase, message, classes, attributes }) => {
+  return Message({
+    params: {
+      message,
+      classes,
+      attributes,
+    },
+  });
+};
+
+export const Standard = Template.bind({});
+Standard.args = {
+  message: "Please note this page references hunger strikes and force feeding, which some people may find upsetting.",
+};

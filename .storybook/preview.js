23a04b90d61547d6e9128591f6abdb645fbdf96a
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-08-10 12:59:07 +0100

    Add support for WCAG AAA testing/reporting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "../src/nationalarchives/all.scss";
+import axe from "axe-core";
 import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 // Also need to change $support-colour-schemes in src/nationalarchives/_features.scss
@@ -25,10 +26,36 @@ const customViewports = {
   // },
 };
 
+const enableRulesByTag = (tags = []) => {
+  const allRules = axe.getRules();
+  return allRules.map((rule) =>
+    tags.some((t) => rule.tags.includes(t))
+      ? { id: rule.ruleId, enabled: true }
+      : { id: rule.ruleId, enabled: false },
+  );
+};
+
+const checkWCAG21AAA = false;
+
 export const parameters = {
   actions: {
     disable: true,
   },
   viewport: { viewports: customViewports },
   options: { showPanel: true },
+  a11y: {
+    config: {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true,
+      },
+      rules: enableRulesByTag([
+        "wcag2a",
+        "wcag21a",
+        "wcag2aa",
+        "wcag21aa",
+        ...(checkWCAG21AAA ? ["wcag2aaa", "wcag21aaa"] : []),
+      ]),
+    },
+  },
 };

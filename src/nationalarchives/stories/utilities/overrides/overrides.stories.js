c791a915f19e934d4f1db3adbec2c20952d20689
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-08-21 10:05:54 +0100

    Responsive overrides for spacing

--- src/nationalarchives/stories/utilities/overrides/overrides.stories.js
+++ src/nationalarchives/stories/utilities/overrides/overrides.stories.js
@@ -1,14 +1,7 @@
 const argTypes = {
   marginTop: {
     control: "radio",
-    options: [
-      "none",
-      // "xs",
-      "s",
-      "m",
-      "l",
-      "xl",
-    ],
+    options: ["none", "xs", "s", "m", "l", "xl"],
   },
 };
 
@@ -22,12 +15,13 @@ const Template = ({ marginTop }) => {
     marginTop === "none"
       ? `tna-!--no-margin-top`
       : `tna-!--margin-top-${marginTop}`;
-  return `<p class="tna-!--no-margin-bottom">Lorem ipsum</p>
-    <p class="tna-!--no-margin-bottom tna-!--margin-top-s">Lorem ipsum (tna-!--margin-top-s)</p>
-    <p class="tna-!--no-margin-bottom tna-!--margin-top-m">Lorem ipsum (tna-!--margin-top-m)</p>
-    <p class="tna-!--no-margin-bottom tna-!--margin-top-l">Lorem ipsum (tna-!--margin-top-l)</p>
-    <p class="tna-!--no-margin-bottom tna-!--margin-top-xl">Lorem ipsum (tna-!--margin-top-xl)</p>
-    <p class="tna-!--no-margin-bottom ${marginClass}">Lorem ipsum (${marginClass})</p>`;
+  return `<p class="tna-!--margin-bottom-xl">Lorem ipsum (tna-!--margin-bottom-xl)</p>
+    <p class="tna-!--margin-bottom-l">Lorem ipsum (tna-!--margin-bottom-l)</p>
+    <p class="tna-!--margin-bottom-m">Lorem ipsum (tna-!--margin-bottom-m)</p>
+    <p class="tna-!--margin-bottom-s">Lorem ipsum (tna-!--margin-bottom-s)</p>
+    <p class="tna-!--margin-bottom-xs">Lorem ipsum (tna-!--margin-bottom-xs)</p>
+    <p class="tna-!--no-margin-bottom">Lorem ipsum (tna-!--no-margin-bottom)</p>
+    <p class="${marginClass}">Lorem ipsum (${marginClass})</p>`;
 };
 
 export const Margin = Template.bind({});

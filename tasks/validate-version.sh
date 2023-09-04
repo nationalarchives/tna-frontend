889f8256d64ed2a0e9e97fea904feabbf60ab1e8
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-09-04 14:48:30 +0100

    Add debug in validate-version.sh

--- tasks/validate-version.sh
+++ tasks/validate-version.sh
@@ -3,6 +3,9 @@
 set -e
 CURRENT_VERSION=$(node -p "require('./package.json').version")
 PACKAGE_LOCK_VERSION=$(node -p "require('./package-lock.json').version")
+echo "Version in package.json:      $CURRENT_VERSION"
+echo "Version in package-lock.json: $PACKAGE_LOCK_VERSION"
+echo "Version to check against:     $1"
 if [ "$CURRENT_VERSION" != "$PACKAGE_LOCK_VERSION" ]; then
   echo "Version $CURRENT_VERSION in package.json doesn't match version $PACKAGE_LOCK_VERSION in package-lock.json."
   exit 1

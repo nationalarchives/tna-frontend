b477ae0988e725be592dffe823cc6e6af114e1f0
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-10-12 18:25:52 +0100

    Remove column layout from breadcrumbs

--- src/nationalarchives/components/breadcrumbs/breadcrumbs.stories.js
+++ src/nationalarchives/components/breadcrumbs/breadcrumbs.stories.js
@@ -1,5 +1,8 @@
 import Breadcrumbs from "./template.njk";
 import macroOptions from "./macro-options.json";
+import { expect } from "@storybook/jest";
+import { within, userEvent } from "@storybook/testing-library";
+import { customViewports } from "../../../../.storybook/viewports";
 
 const argTypes = {
   items: { control: "object" },
@@ -57,7 +60,68 @@ Standard.args = {
 };
 
 export const NoCollapse = Template.bind({});
+NoCollapse.parameters = {
+  viewport: {
+    defaultViewport: "small",
+  },
+  chromatic: {
+    viewports: [customViewports["small"].styles.width.replace(/px$/, "")],
+  },
+};
 NoCollapse.args = {
   ...Standard.args,
   noCollapse: true,
+};
+
+export const Mobile = Template.bind({});
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: "small",
+  },
+  chromatic: {
+    viewports: [customViewports["small"].styles.width.replace(/px$/, "")],
+  },
+};
+Mobile.args = {
+  ...Standard.args,
+};
+
+export const MobileExpanded = Template.bind({});
+MobileExpanded.parameters = {
+  viewport: {
+    defaultViewport: "small",
+  },
+  chromatic: {
+    viewports: [customViewports["small"].styles.width.replace(/px$/, "")],
+  },
+};
+MobileExpanded.args = {
+  ...Standard.args,
+};
+MobileExpanded.play = async ({ args, canvasElement, step }) => {
+  const canvas = within(canvasElement);
+
+  const $module = document.querySelector('[data-module="tna-breadcrumbs"]');
+  const $items = args.items.map((item) => canvas.getByText(item.text));
+  const $expandButton = document.querySelector(
+    ".tna-breadcrumbs__item--expandable button",
+  );
+
+  await expect($items[0]).toBeVisible();
+  await expect($items[1]).not.toBeVisible();
+  await expect($items[2]).not.toBeVisible();
+  await expect($items[3]).not.toBeVisible();
+  await expect($items[4]).toBeVisible();
+  await expect($expandButton).toBeVisible();
+
+  await userEvent.click($expandButton);
+
+  await expect($items[0]).toBeVisible();
+  await expect($items[1]).toBeVisible();
+  await expect($items[2]).toBeVisible();
+  await expect($items[3]).toBeVisible();
+  await expect($items[4]).toBeVisible();
+  await expect($expandButton).not.toBeVisible();
+
+  await expect($module).toHaveFocus();
 };

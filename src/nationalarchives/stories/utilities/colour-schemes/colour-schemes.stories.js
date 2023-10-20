844fae772ad96067fd4a80495e9ebccb77599ef1
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-10-20 13:18:47 +0100

    Fix more headings and spacing

--- src/nationalarchives/stories/utilities/colour-schemes/colour-schemes.stories.js
+++ src/nationalarchives/stories/utilities/colour-schemes/colour-schemes.stories.js
@@ -486,17 +486,17 @@ const Template = ({ theme, accent }) => {
                 {
                   id: "unique-id-a",
                   title: "Alpha section",
-                  body: '<h2 class="tna-heading">Alpha title</h2><p>Lorem ipsum</p>',
+                  body: '<h2 class="tna-heading-l">Alpha title</h2><p>Lorem ipsum</p>',
                 },
                 {
                   id: "unique-id-b",
                   title: "Beta section",
-                  body: '<h2 class="tna-heading">Beta title</h2><p>Lorem ipsum</p>',
+                  body: '<h2 class="tna-heading-l">Beta title</h2><p>Lorem ipsum</p>',
                 },
                 {
                   id: "unique-id-c",
                   title: "Gamma section",
-                  body: '<h2 class="tna-heading">Gamma title</h2><p>Lorem ipsum</p>',
+                  body: '<h2 class="tna-heading-l">Gamma title</h2><p>Lorem ipsum</p>',
                 },
               ],
               classes: "tna-tabs--demo",

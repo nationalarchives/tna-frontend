5775a2b41b641f6b6c2fa3c20d449f5fe536bb48
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2023-10-20 18:37:50 +0100

    Reduce space above lists

--- src/nationalarchives/stories/utilities/colour-schemes/colour-schemes.stories.js
+++ src/nationalarchives/stories/utilities/colour-schemes/colour-schemes.stories.js
@@ -398,12 +398,13 @@ const Template = ({ theme, accent }) => {
       <hr>
       <div class="tna-container tna-section">
         <div class="tna-column tna-column--full">
-          <p class="tna-!--no-margin-bottom">Lorem ipsum</p>
-          <p class="tna-!--no-margin-bottom tna-!--padding-top-xs">Lorem ipsum</p>
-          <p class="tna-!--no-margin-bottom tna-!--padding-top-s">Lorem ipsum</p>
-          <p class="tna-!--no-margin-bottom tna-!--padding-top-m">Lorem ipsum</p>
-          <p class="tna-!--no-margin-bottom tna-!--padding-top-l">Lorem ipsum</p>
-          <p class="tna-!--no-margin-bottom tna-!--padding-top-xl">Lorem ipsum</p>
+          <p>Lorem ipsum</p>
+          <p class="tna-!--no-margin-top">Lorem ipsum (tna-!--no-margin-top)</p>
+          <p class="tna-!--margin-top-xs">Lorem ipsum (tna-!--margin-top-xs)</p>
+          <p class="tna-!--margin-top-s">Lorem ipsum (tna-!--margin-top-s)</p>
+          <p class="tna-!--margin-top-m">Lorem ipsum (tna-!--margin-top-m)</p>
+          <p class="tna-!--margin-top-l">Lorem ipsum (tna-!--margin-top-l)</p>
+          <p class="tna-!--margin-top-xl">Lorem ipsum (tna-!--margin-top-xl)</p>
         </div>
       </div>
       ${Hero({

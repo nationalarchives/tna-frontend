d5c96b1672931079c5fba3a31931ae902c703a80
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-02-09 10:56:20 +0000

    Remove app-specific tracking, export helpers, change GA4 implementation

--- src/nationalarchives/analytics.mjs
+++ src/nationalarchives/analytics.mjs
@@ -1,5 +1,9 @@
 import Cookies from "./lib/cookies.mjs";
-import { getXPathTo, valueGetters } from "./lib/analytics-helpers.mjs";
+import {
+  getXPathTo,
+  getClosestHeading,
+  valueGetters,
+} from "./lib/analytics-helpers.mjs";
 import BreadcrumbAnalytics from "./components/breadcrumbs/analytics.js";
 import HeaderAnalytics from "./components/header/analytics.js";
 import HeroAnalytics from "./components/hero/analytics.js";
@@ -118,16 +122,16 @@ class EventTracker {
 
   /** @protected */
   recordEvent(eventName, data) {
-    this.events.push({ event: eventName, data });
+    this.events.push({ event: eventName, "tna.data": data });
   }
 
   /** @protected */
   getTnaMetaTags() {
     return Object.fromEntries(
       Array.from(
-        document.head.querySelectorAll("meta[name^='tna:'][content]"),
+        document.head.querySelectorAll("meta[name^='tna.'][content]"),
       ).map(($metaEl) => [
-        $metaEl.getAttribute("name").replace(/^tna:/, ""),
+        $metaEl.getAttribute("name"),
         $metaEl.getAttribute("content"),
       ]),
     );
@@ -166,15 +170,17 @@ class GA4 extends EventTracker {
 
   /** @protected */
   recordEvent(eventName, data) {
-    const ga4Data = { event: eventName, data };
-    window.dataLayer.push(ga4Data);
-    // console.log(window.dataLayer);
+    const ga4Data = { event: eventName, "tna.data": data };
+    this.pushToDataLayer(ga4Data);
   }
 
   /** @protected */
   gtag() {
-    window.dataLayer.push(arguments);
-    // console.log(window.dataLayer);
+    this.pushToDataLayer(arguments);
+  }
+
+  pushToDataLayer(data) {
+    window.dataLayer.push(data);
   }
 
   /** @protected */
@@ -183,18 +189,19 @@ class GA4 extends EventTracker {
       window["ga-disable-GA_MEASUREMENT_ID"] = false;
       this.trackingEnabled = true;
       if (!this.trackingCodeAdded) {
+        this.pushToDataLayer({
+          "gtm.start": new Date().getTime(),
+          event: "gtm.js",
+        });
+        const firstScript = document.getElementsByTagName("script")[0];
         const script = document.createElement("script");
-        script.setAttribute("async", true);
-        script.setAttribute(
-          "src",
-          `https://www.googletagmanager.com/gtm.js?id=${this.gTagId}&l=dataLayer`,
-        );
-        // document.head.appendChild(script);
-        this.gtag("js", new Date());
+        script.async = true;
+        script.src = `https://www.googletagmanager.com/gtm.js?id=${this.gTagId}&l=dataLayer`;
+        firstScript.parentNode.insertBefore(script, firstScript);
         this.trackingCodeAdded = true;
-        window.dataLayer.push(this.getTnaMetaTags());
+        this.pushToDataLayer(this.getTnaMetaTags());
       }
-      this.gtag("set", { allow_ad_personalization_signals: false });
+      this.gtag("set", { allow_google_signals: true });
     }
   }
 
@@ -202,68 +209,12 @@ class GA4 extends EventTracker {
   disableTracking() {
     if (this.trackingEnabled) {
       window["ga-disable-GA_MEASUREMENT_ID"] = true;
-      this.gtag("set", { allow_ad_personalization_signals: true });
+      this.gtag("set", { allow_google_signals: false });
       this.trackingEnabled = false;
     }
   }
 }
 
-/*
- * ==========================================
- * TEMP TESTING
- * ==========================================
- */
-const analytics = new GA4("test");
+const helpers = { getXPathTo, getClosestHeading, valueGetters };
 
-analytics.addListener(".etna-article__sidebar", "sidebar", [
-  {
-    eventName: "scection_jump",
-    targetElement: ".etna-article__sidebar-item",
-    on: "click",
-    data: {
-      value: valueGetters.text,
-    },
-  },
-]);
-
-analytics.addListener(".etna-article", "article", [
-  {
-    eventName: "toggle_section",
-    targetElement: ".etna-article__section-button",
-    on: "click",
-    data: {
-      // eslint-disable-next-line no-unused-vars
-      state: ($el, $scope, event) => {
-        const expanded = $el.getAttribute("aria-expanded");
-        if (expanded === null) {
-          return null;
-        }
-        return expanded.toString() === "true" ? "opened" : "closed";
-      },
-      value: valueGetters.text,
-    },
-  },
-]);
-
-analytics.addListener(document.documentElement, "doc", [
-  {
-    eventName: "double_click",
-    on: "dblclick",
-    data: {
-      state: ($el, $scope, event) => getXPathTo(event.target),
-      value: ($el, $scope, event) => event.target.innerHTML,
-    },
-  },
-]);
-
-analytics.addListener(document.getElementById("tna-form__search"), "search", [
-  {
-    eventName: "search_term_blur",
-    on: "blur",
-    data: {
-      value: valueGetters.value,
-    },
-  },
-]);
-
-export { EventTracker, GA4 };
+export { EventTracker, GA4, helpers };

4096bff6dd46b49c4a138ed1c6c6e512a6a407a0
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-07-12 14:20:09 +0100

    Edit sed command in package build

--- tasks/build-package.sh
+++ tasks/build-package.sh
@@ -7,4 +7,4 @@ npm run compile:scripts
 cp -R src/nationalarchives package.json package-lock.json README.md LICENCE govuk-prototype-kit.config.json package
 mkdir package/config
 cp -R .babelrc.json .eslintrc.js .htmlvalidate.json stylelint.config.js package/config
-sed -i '' -e 's/, "plugin:storybook\/recommended"//g' package/config/.eslintrc.js
+sed -i -e 's/, "plugin:storybook\/recommended"//g' package/config/.eslintrc.js

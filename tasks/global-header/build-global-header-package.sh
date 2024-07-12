f2db82c33b4100d0bb435d6713b8a215c1c7c185
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-07-12 14:16:13 +0100

    Try running build-package.sh directly in GitHub workflows

--- tasks/global-header/build-global-header-package.sh
+++ tasks/global-header/build-global-header-package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-npm run build:package
+sh ./tasks/build-package.sh
 rm -fR package-global-header
 mkdir package-global-header
 cp -R package/nationalarchives/global-header-package.css package/nationalarchives/global-header-package.css.map package/nationalarchives/all.js package/nationalarchives/all.js.map package/nationalarchives/analytics.js package/nationalarchives/analytics.js.map LICENCE package-global-header

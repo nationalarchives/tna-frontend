d7692d682ed8dd6c9eb630e09f64921f26a9feeb
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-06-02 16:50:46 +0100

    Enhance stylelint configuration by adding order rules for at-rules and properties

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,60 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["@nationalarchives/stylelint-config"],
+  rules: {
+    "order/order": [
+      [
+        "dollar-variables",
+        {
+          type: "at-rule",
+          name: "mixin",
+          hasBlock: true,
+        },
+        {
+          type: "at-rule",
+          name: "include",
+          parameter: "spacing",
+        },
+        {
+          type: "at-rule",
+          name: "include",
+          parameter: "typography",
+        },
+        {
+          type: "at-rule",
+          name: "include",
+          parameter: "colour",
+        },
+        {
+          type: "at-rule",
+          name: "include",
+          parameter: "borders",
+        },
+        {
+          type: "at-rule",
+          name: "include",
+          parameter: "a11y",
+        },
+        "custom-properties",
+        "declarations",
+        "rules",
+        "at-rules",
+        {
+          type: "at-rule",
+          name: "include",
+          parameter: "media.on-.*",
+          hasBlock: true,
+        },
+        {
+          type: "at-rule",
+          name: "include",
+          parameter: "colour.on-.*",
+          hasBlock: true,
+        },
+      ],
+      {
+        unspecified: "bottom",
+      },
+    ],
+  },
 };
